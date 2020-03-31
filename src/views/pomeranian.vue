9e2e4dd046875b807074705a14ede0e25dd9c315
oshioshi <y.oshita0110@gmail.com>
2020-03-31 09:59:36 +0900

    feat : .htaccess追加

--- src/views/pomeranian.vue
+++ src/views/pomeranian.vue
@@ -8,10 +8,6 @@
     <p class="bodyMsg">活発でよく遊び、フレンドリーな性格をしています</p>
     <dogInfo jsonFileName = "pomeranian.json"/>
 
-    <div class="a8">
-      <a href="https://px.a8.net/svt/ejp?a8mat=35SGE4+9HQU0I+46HI+626XT" rel="nofollow">
-      <img border="0" width="300" height="250" alt="" src="https://www21.a8.net/svt/bgt?aid=191124220574&wid=001&eno=01&mid=s00000019503001018000&mc=1"></a>
-    </div>
 
   </div> 
 </template>
--- src/views/shibainu.vue
+++ src/views/shibainu.vue
@@ -8,11 +8,6 @@
     <p class="bodyMsg">賢く勇敢な性格で、飼い主に忠実です</p>
     <dogInfo jsonFileName = "shibainu.json"/>
 
-<div class="a8">
-  <a href="https://px.a8.net/svt/ejp?a8mat=35SGE4+9H5EEQ+46DW+BXYE9" rel="nofollow"> 
-  <img border="0" width="300" height="250" alt="" src="https://www29.a8.net/svt/bgt?aid=191124220573&wid=001&eno=01&mid=s00000019490002006000&mc=1"></a>
-</div>
-
   </div> 
 </template>
 
